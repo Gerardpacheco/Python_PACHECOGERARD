1d846bbe1b17e4a4000fe5f98cdae33a5a4a6413
GerardPacheco <pachecosanchezgerardmauricio@gmail.com>
2024-04-09 12:59:54 -0500

    dia1

---
+++ DIA1/main.py
@@ -0,0 +1,41 @@
+#--------------------------------
+#--------Dia1 CHEAT SHEET PYTHON-------
+#---------------------------------
+#imprinir hola mundo
+print("hola mundoooooooo !!!!")
+
+#datos primitivos
+#numero
+numero=10 #definimos
+print(numero)#imprimir variable
+print(type(numero))
+
+#Decimal
+numeritoDecimal = 1.1
+print(numeritoDecimal)
+print(type(numeritoDecimal))
+
+#Booleano
+miBooleanito = True
+print(miBooleanito)
+print(type(miBooleanito))
+
+#Cadena de texto
+texto = "MI TIBU"
+print(texto)
+print(type(texto))
+
+#ingresa por teclado la informacion(input)
+edad=input("ingresa tu edad")
+edad=(int (edad))
+print("tu edad es: ",edad, "años")
+
+numero=input("ingresa un numero")
+numero=(int (numero))
+print("tu numero ingresado es ",numero,)
+
+#conversion de datos primitivos 
+#bucles for y while
+for i in range(5):
+    print(i)
+# Desarrollado pr Gerard Mauricio Pacheco Sanchez TI 1125758533
