3cbb9c4ca1133dbf2316fd8190ca246f58ead667
GerardPacheco <pachecosanchezgerardmauricio@gmail.com>
2024-04-10 12:56:38 -0500

    ejercicioshoy

---
+++ DIA2/ejercicio#1_PachecoGerard.py
@@ -0,0 +1,9 @@
+#secuencia de fibonacci
+print("breve explicacion de la serie fibonacci:")
+print("La secuencia de Fibonacci es una sucesión infinita de números naturales, descrita por primera vez por el matemático italiano Fibonacci \n en el siglo XIII. Esta serie numérica empieza con 0 y 1, siguiendo con la suma de los dos números anteriores hasta el infinito: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233.")
+numero=input("ingresa un numero hasta donde ira la serie fibonacci por favor")
+numero=int((numero))
+
+for i in range(1,numero,1+1)
+#Desarrollado por Gerard Mauricio Pacheco Sanchez
+#TI:1125758533
