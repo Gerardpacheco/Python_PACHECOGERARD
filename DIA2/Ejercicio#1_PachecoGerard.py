d5771eb560c4cd7e0f06a89dc703dcc2746c106b
Gerardpacheco <pachecosanchezgerardmauricio@gmail.com>
2024-04-10 22:39:22 -0500

    ejercicioDos

--- DIA2/Ejercicio#1_PachecoGerard.py
+++ DIA2/Ejercicio#1_PachecoGerard.py
@@ -1,8 +1,8 @@
 #serie de fibonacci
 print("breve explicacion de la secuencia fibonacci:")
 print("En matemáticas, la sucesión de Fibonacci (a veces  llamada serie de Fibonacci) es la sucesión infinita de números naturales.La sucesión comienza con los números 0 y 1, y a partir de estos, cada elemento es la suma de los dos anteriores.")
-
-while True:
+#ciclo para repetir si la respuesta es si sino termina el ciclo
+while True: #ingresamos el numero hasta donde se generara la seriee fibonacci y hacemos las operaciones para calcularla, hasta el numero donde se generara
     n = int(input("Por favor ingresa el número hasta donde se generará la serie Fibonacci: "))
     a = 0
     b = 1
@@ -15,7 +15,7 @@ while True:
             print(b, end=",")
             b += a 
         c += 1
-    jugar_nuevamente = input("\n¿Quieres jugar de nuevo? (si/no): ")
+    jugar_nuevamente = input("\n¿Quieres jugar de nuevo? (si/no): ")#pregunta si quieres  jugar de nuevo con el programa
     if jugar_nuevamente.lower() != "si":
         break
         #Desarrolado por Gerard Mauricio Pacheco Sanchez
--- DIA2/Ejercicio#2_PachecoGerard.py
+++ DIA2/Ejercicio#2_PachecoGerard.py
@@ -1,4 +1,4 @@
-#numero Aleatorio sin limite de intentos
+#numero Aleatorio con limite de intentos de 3 minimos
 import random
 #Creamos numeros aleatorios entre 1 y 100
 numaleatorio=random.randint(1,100)
@@ -6,7 +6,7 @@ numaleatorio=random.randint(1,100)
 print("adivina el numero escondidio entre 1 y 100 en 3 intentos")
 #contador de intentos
 intentoss= 1
-#mientras intentos sea mayor o igual a cero, le preguntamos un nuevo numero 
+#mientras intentos sea mayor o igual a tres, le preguntamos un nuevo numero 
 while intentoss <= 3:
     print(f"intento N° {intentoss}")
     Nuser=int(input("ingresa un numero "))
@@ -20,7 +20,7 @@ while intentoss <= 3:
         break
 if Nuser==numaleatorio: # si el numero ingresado por el usuario es igual al escondido, imprimimos que adivino y en cuantos intentos 
     print("ADIVINASTES. El numero secreto el cual era ", numaleatorio, " y lo adivinastes en ",intentoss-1, " intentos" )
-else:
+else: # si se acaban los intentos mostramos el numero secreto
     print(f"NO ADIVINASTES , el numero secreto era {numaleatorio} ")
 
 ## Desaroolado por Gerard Mauricio Pacheco Sanchez
