3789fa79b7d04aaa4ead1b57091f5a97ef24eccb
Gerardpacheco <pachecosanchezgerardmauricio@gmail.com>
2024-04-15 21:10:23 -0500

    ejercicio1

---
+++ DIA5/Ejercicio1_PachecoGerard.py
@@ -0,0 +1,12 @@
+#pedimos los numeros al usuario para guardarlos en la lista
+n=(input("ingrese los numeros separados por coma"))
+numeros=[n]
+print(numeros)
+#pedimos por que numero los va a dividir
+divisor=int(input("por que numeros los piensas dividir"))
+num=len(numeros)#convertimos la lista a una longitud
+for i in range(num-1):#bucle para repetir de la longitud de datos ingresados 
+    
+
+
+#Desarrollado por Gerard Mauricio Pacheco Sanchez
